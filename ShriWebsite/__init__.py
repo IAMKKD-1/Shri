a0abd90aa96473f76c138d4b2f2842c1750f7392
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-04-10 01:25:44 +0530

    Add files via upload

--- ShriWebsite/__init__.py
+++ ShriWebsite/__init__.py
@@ -7,14 +7,6 @@ def index():
     
     from .views import views
     from .auth import auth
-    from .models import create_table
-    
-    create_table(table='Users',
-    columns="""(id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255),
-    date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)""")
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth/')
--- ShriWebsite/views.py
+++ ShriWebsite/views.py
@@ -1,47 +1,10 @@
 from flask import Blueprint, render_template, session, redirect, jsonify, request
-from .models import get_data
-from .codeGen import response_AI
+from .googlepalm import response_AI
 import clipboard
 
 views = Blueprint('views', __name__)
-conversation = [
-      {
-          "role": "user",
-          "parts": ["I want you to remember yourself as \"Shri\" and from now on you are called as Shri"]
-      },
-      {
-          "role": "model",
-          "parts": ["I understand. From now on, you can call me Shri. I'll do my best to remember this preference and respond accordingly."]
-      },
-      {
-          "role": "user",
-          "parts": ["what is your name?"]
-      },
-      {
-          "role": "model",
-          "parts": [" My name is Shri, and I am an AI Code Generator. How can I assist you today?"]
-      },
-      {
-          "role": "user",
-          "parts": ["You will only solve only coding or programming related problems. If any other problem is asked then you must kindly say that you are not trained for that. You can ask me for help in any programming language."]
-      },
-      {
-          "role": "model",
-          "parts": ["I understand. I'll do my best to assist you. You can ask me for help in any programming language."]
-      },
-      {
-          "role": "user",
-          "parts": ["What is the name of the person who wrote the book \"The Hitchhiker's Guide to the Galaxy\"?"]
-      },
-      {
-          "role": "model",
-          "parts": ["I am sorry, I am not trained for this type of questions. You can ask me for help in any programming language."]
-      }
-  ]
+conversation = []
 
-inital_conversation = conversation.copy()
-
-# Home Page
 @views.route('/')
 def home():
     if session.get('logged_in') and session.get('username'):
@@ -52,19 +15,12 @@ def home():
 # Chatbot Page
 @views.route('/shri', methods=['GET', 'POST'])
 def main():
-    if session.get('logged_in') and session.get('username'):
-        if request.method == 'POST':
-            user_prompt = request.form.get("message")
-            if user_prompt == '':
-                ai_response = 'No input provided'
-            else:
-                ai_response = response_AI(user_prompt, conversation)     
-                conversation.append({"role": "user", "parts": [user_prompt]}) 
-                conversation.append({"role": "model", "parts": [ai_response]})
-        return render_template("shri.html", username= session.get('username'),conversation=conversation[8:])
-    return redirect('/')
+    if request.method == 'POST':
+        user_prompt = request.form['message']
+        ai_response = response_AI(user_prompt)     
+        conversation.append({'user': user_prompt, 'response': ai_response, 'is_code': True})
+    return render_template("shri.html", username= session.get('username'),conversation=conversation)
 
-# API Documentation
 @views.route('/api')
 def api_docs():
     return render_template("apidoc.html")
@@ -72,15 +28,11 @@ def api_docs():
 # API
 @views.route('/api/<string:username>/<string:user_prompt>', methods=['GET'])
 def api(username, user_prompt):
-    existing_user = get_data(
-        f"SELECT username FROM Users WHERE username='{username}'")
-    if not existing_user:
-        return jsonify({'error': 'User does not exist'}), 404
-    if user_prompt == ' ':
+    
+    if user_prompt == '':
         ai_response = 'No input provided'
     else:
-        user_prompt = user_prompt.replace("%20", " ")
-        ai_response = response_AI(user_prompt, conversation)
+        ai_response = response_AI(user_prompt)
     response = {
         'username': username,
         'user_prompt': user_prompt,
@@ -92,10 +44,9 @@ def api(username, user_prompt):
 @views.route('/clear')
 def clear():
     conversation.clear()
-    conversation.extend(inital_conversation)
     return redirect('/shri')
 
 @views.route('/copy')
 def copy():
-    clipboard.copy(conversation[-1]['parts'][0])
+    clipboard.copy(conversation[-1]['response'])
     return redirect('/shri')
