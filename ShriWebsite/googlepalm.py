8c1f2ddbdf507e527c2e392478ffa9e19fc1dbcb
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-04-10 01:41:08 +0530

    Add files via upload

--- ShriWebsite/googlepalm.py
+++ ShriWebsite/googlepalm.py
@@ -1,8 +1,11 @@
 import google.generativeai as palm
+from dotenv import load_dotenv
+import os
 
+api_key = os.environ.get("MODEL_KEY")
 
 def response_AI(prompt):
-    palm.configure(api_key='AIzaSyATkhdCSm1lk0g4IQbWB5lJ3xpUpE6tHR4')
+    palm.configure(api_key=api_key)
     completion = palm.generate_text(
         model="models/text-bison-001",
         prompt=prompt,
